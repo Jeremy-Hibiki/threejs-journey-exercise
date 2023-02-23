d91023340992649ca8faa66317a149376ccfebee
Jeremy <2687817540@qq.com>
2023-02-24 05:04:05 +0800

    add: 34-intro-and-loading-progress

---
+++ exercise/34-intro-and-loading-progress/src/script.ts
@@ -0,0 +1,188 @@
+import gsap from 'gsap';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+/**
+ * Loaders
+ */
+const loadingBarDom = document.querySelector('.loading-bar') as HTMLDivElement;
+const loadingManager = new THREE.LoadingManager(
+  () => {
+    gsap.delayedCall(0.1, () => {
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      gsap.to(overlay.material.uniforms.uAlpha, {
+        duration: 3,
+        value: 0,
+      });
+
+      loadingBarDom.style.transform = '';
+      loadingBarDom.classList.add('loading-bar__end');
+    });
+  },
+  (url, loaded, total) => {
+    loadingBarDom.style.transform = `scaleX(${loaded / total})`;
+  },
+);
+
+const gltfLoader = new GLTFLoader(loadingManager);
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager);
+
+/**
+ * Base
+ */
+// Debug
+const debugObject = {
+  envMapIntensity: 2.5,
+};
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement;
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Overlay
+ */
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1);
+const overlayMaterial = new THREE.ShaderMaterial({
+  transparent: true,
+  uniforms: {
+    uAlpha: { value: 1 },
+  },
+  vertexShader: /* glsl */ `
+    void main() {
+      gl_Position = vec4(position, 1.);
+    }
+  `,
+  fragmentShader: /* glsl */ `
+    uniform float uAlpha;
+    void main() {
+      gl_FragColor = vec4(0., 0., 0., uAlpha);
+    }
+  `,
+});
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial);
+scene.add(overlay);
+
+/**
+ * Update all materials
+ */
+const updateAllMaterials = () => {
+  scene.traverse((child) => {
+    if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+      // child.material.envMap = environmentMap
+      child.material.envMapIntensity = debugObject.envMapIntensity;
+      child.material.needsUpdate = true;
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+};
+
+/**
+ * Environment map
+ */
+const environmentMap = cubeTextureLoader.load([
+  '/textures/environmentMaps/0/px.jpg',
+  '/textures/environmentMaps/0/nx.jpg',
+  '/textures/environmentMaps/0/py.jpg',
+  '/textures/environmentMaps/0/ny.jpg',
+  '/textures/environmentMaps/0/pz.jpg',
+  '/textures/environmentMaps/0/nz.jpg',
+]);
+
+environmentMap.encoding = THREE.sRGBEncoding;
+
+scene.background = environmentMap;
+scene.environment = environmentMap;
+
+/**
+ * Models
+ */
+gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', (gltf) => {
+  gltf.scene.scale.set(10, 10, 10);
+  gltf.scene.position.set(0, -4, 0);
+  gltf.scene.rotation.y = Math.PI * 0.5;
+  scene.add(gltf.scene);
+
+  updateAllMaterials();
+});
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.normalBias = 0.05;
+directionalLight.position.set(0.25, 3, -2.25);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.set(4, 1, -4);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.physicallyCorrectLights = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 3;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const tick = () => {
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
