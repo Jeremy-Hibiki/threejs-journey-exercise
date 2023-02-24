c337d3a1daea9623ab6f1692903003004f3574ac
Jeremy <2687817540@qq.com>
2023-02-25 03:49:24 +0800

    add: 35-mixing-html-and-webgl

---
+++ exercise/35-mixing-html-and-webgl/vite.config.ts
@@ -0,0 +1,22 @@
+import autoprefixer from 'autoprefixer';
+import postcssNesting from 'postcss-nesting';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: 'src/',
+  publicDir: '../static/',
+  base: './',
+  css: {
+    postcss: {
+      plugins: [autoprefixer, postcssNesting],
+    },
+  },
+  server: {
+    host: true,
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+});
