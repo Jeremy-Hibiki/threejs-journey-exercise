ce05583aa9f232e69e7087193972aefefd8a20cc
Jeremy <2687817540@qq.com>
2023-02-24 18:37:37 +0800

    chore: update linter

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: [require.resolve('@umijs/lint/dist/config/stylelint')],
+  extends: [require.resolve('@umijs/lint/dist/config/stylelint'), 'stylelint-config-recess-order'],
   rules: {
     'selector-class-pattern': null,
   },
