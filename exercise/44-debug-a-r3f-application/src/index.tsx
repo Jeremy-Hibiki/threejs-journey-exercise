565de2c5ea8b3bc4255168c6e34b873a10af98df
Jeremy <2687817540@qq.com>
2023-02-26 07:09:31 +0800

    add: 44-debug-a-r3f-application

---
+++ exercise/44-debug-a-r3f-application/src/index.tsx
@@ -0,0 +1,24 @@
+import { Canvas } from '@react-three/fiber';
+import { Leva } from 'leva';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import Experience from './Experience';
+import './style.css';
+
+const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement);
+
+root.render(
+  <StrictMode>
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Experience />
+    </Canvas>
+    <Leva collapsed />
+  </StrictMode>,
+);
