b60dd07fd62f5a2399c75d56fb1f76928ee56b91
Jeremy <2687817540@qq.com>
2023-03-05 05:06:48 +0800

    add: 47-3d-text-with-r3f

---
+++ exercise/47-3d-text-with-r3f/src/index.tsx
@@ -0,0 +1,19 @@
+import { Canvas } from '@react-three/fiber';
+import ReactDOM from 'react-dom/client';
+import Experience from './Experience';
+import './style.css';
+
+const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement);
+
+root.render(
+  <Canvas
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 200,
+      position: [4, -2, 6],
+    }}
+  >
+    <Experience />
+  </Canvas>,
+);
